ce1a8ad582d72d01770e47b4db049408389aea90
mammothb <mammothb+git@gmail.com>
2018-12-19 09:59:47 +0800

    added check for empty option field, request password auth for gfycat

--- clipsapp.py
+++ clipsapp.py
@@ -86,6 +86,10 @@ class ClipsApp(QMainWindow):
             self._info_group.set_info(info)
 
     def set_options(self, start_time, duration, num_clip):
+        if (start_time is None or duration is None or num_clip is None or
+                not start_time or not duration or not num_clip):
+            self._info_group.set_info("ERROR: Missing options")
+            return
         success, info = self._clip_maker.set_options(start_time, duration,
                                                      num_clip)
         if success:
--- gfycatuploader.py
+++ gfycatuploader.py
@@ -31,8 +31,11 @@ class GfycatUploader(QObject):
         super().__init__()
         cwd = os.path.realpath(os.path.dirname(sys.argv[0]))
         with open(os.path.join(cwd, "apikey.txt"), "r") as api_file:
-            self.client_id, self.client_secret = [
-                s.split("=")[-1].strip() for s in api_file.readlines()]
+            lines = api_file.readlines()
+            self.client_id = lines[0].split("=")[-1].strip()
+            self.client_secret = lines[1].split("=")[-1].strip()
+            self.username = lines[2].split("=")[-1].strip()
+            self.password = lines[3].split("=")[-1].strip()
 
     def emit_error(self, error_message, status_code=None):
         error = GfycatUploaderError(error_message, status_code)
@@ -41,9 +44,11 @@ class GfycatUploader(QObject):
 
     def get_auth_headers(self):
         body = {
-            "grant_type": "client_credentials",
+            "grant_type": "password",
             "client_id": self.client_id,
-            "client_secret": self.client_secret
+            "client_secret": self.client_secret,
+            "username": self.username,
+            "password": self.password
         }
 
         r = requests.post(self.token_endpoint, json=body, timeout=3)
