9b595af2dd213d07daa93d38073bbf325e1fd03e
mammothb <mammothb+git@gmail.com>
2018-12-21 17:06:59 +0800

    implemented processing multiple source files

--- clipmaker.py
+++ clipmaker.py
@@ -8,8 +8,39 @@ from utils import try_parse_int64, try_parse_time
 
 LOG = logging.getLogger("ClipMaker")
 
+def check_source(source_path):
+    cmd_ffprobe = ["ffprobe", "-v", "error", "-show_entries",
+                   "format=duration", "-of",
+                   "default=noprint_wrappers=1:nokey=1", source_path]
+    process = subprocess.Popen(cmd_ffprobe, stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT)
+    stdout, __ = process.communicate()
+    try:
+        source_len = round(float(stdout.decode("utf-8")))
+        is_valid = True
+        target = os.path.splitext(source_path)[0] + ".webm"
+        info_str = "Valid source: {}; Source length: {}".format(source_path,
+                                                                source_len)
+        LOG.info(info_str)
+        return is_valid, target, source_len, info_str
+    except (AttributeError, ValueError):
+        is_valid = False
+        info_str = "Invalid source: {}".format(source_path)
+        LOG.warning(info_str)
+        return is_valid, "", -1.0, info_str
+
+class ClipJob(object):
+    def __init__(self, source_path, target, source_length):
+        self.is_valid = True
+        self.jump = None
+        self.source = source_path
+        self.source_dir = os.path.dirname(source_path)
+        self.source_length = source_length
+        self.target = target
+
 class ClipMaker(object):
     def __init__(self):
+        self.jobs = []
         self.source = None
         self.source_dir = None
         self.source_len = None
@@ -23,71 +54,50 @@ class ClipMaker(object):
         self.jump = None
 
         cwd = os.path.realpath(os.path.dirname(sys.argv[0]))
-        self.config_filename = os.path.join(cwd, "presets.ini")
-        open(self.config_filename, "a").close()
+        self.config_file_name = os.path.join(cwd, "presets.ini")
+        open(self.config_file_name, "a").close()
         self.config = ConfigParser()
-        self.config.read(self.config_filename)
-
-    def check_source(self):
-        if self.source is None:
-            self.is_valid_source = False
-            info_str = "Invalid video file"
-            LOG.warning(info_str)
-            return False, info_str
-
-        cmd_ffprobe = ["ffprobe", "-v", "error", "-show_entries",
-                       "format=duration", "-of",
-                       "default=noprint_wrappers=1:nokey=1", self.source]
-        process = subprocess.Popen(cmd_ffprobe, stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT)
-        stdout, __ = process.communicate()
-        try:
-            self.source_len = round(float(stdout.decode("utf-8")))
-            self.is_valid_source = True
-            self.target = os.path.splitext(self.source)[0] + ".webm"
-            info_str = "Valid source: {}; Source length: {}".format(
-                self.source, self.source_len)
-            LOG.info(info_str)
-            return True, info_str
-        except (AttributeError, ValueError):
-            self.is_valid_source = False
-            info_str = "Invalid video file"
-            LOG.warning(info_str)
-            return False, info_str
+        self.config.read(self.config_file_name)
 
     def check_options(self):
-        success, info = self.check_source()
-        if not success:
-            return success, info
+        if not self.jobs:
+            return [(False, "No source videos")]
+
         if self.start_time is None:
             info_str = "Invalid start time"
             LOG.warning(info_str)
-            return False, info_str
-        if self.start_time > self.source_len:
-            info_str = "Start time exceeds video length"
-            LOG.warning(info_str)
-            return False, info_str
-
-        working_duration = self.source_len - self.start_time
-        if (self.duration is None or self.duration <= 0 or
-                self.duration > working_duration):
+            return [(False, -1, "", info_str)]
+        if self.duration is None or self.duration <= 0:
             info_str = "Invalid duration"
             LOG.warning(info_str)
-            return False, info_str
-
-        trailer_duration = self.num_clip * self.duration
-        if (self.num_clip is None or self.num_clip <= 0 or
-                trailer_duration > working_duration):
+            return [(False, -1, "", info_str)]
+        if self.num_clip is None or self.num_clip <= 0:
             info_str = "Invalid no. of clips"
             LOG.warning(info_str)
-            return False, info_str
+            return [(False, -1, "", info_str)]
 
-        self.jump = working_duration // self.num_clip
-        info_str = ("Creating a {}s trailer with {} clips, each {}s long "
-                    "taken every {}s".format(trailer_duration, self.num_clip,
-                                             self.duration, self.jump))
-        LOG.info(info_str)
-        return True, info_str
+        results = []
+        for idx, job in enumerate(self.jobs):
+            job.is_valid = False
+            working_duration = job.source_length - self.start_time
+            trailer_duration = self.num_clip * self.duration
+            if self.duration > working_duration:
+                info_str = "Invalid duration"
+                LOG.warning("%s: %s", info_str, job.source)
+                results.append((False, idx, job.source, info_str))
+            elif trailer_duration > working_duration:
+                info_str = "Invalid no. of clips"
+                LOG.warning("%s: %s", info_str, job.source)
+                results.append((False, idx, job.source, info_str))
+            else:
+                job.is_valid = True
+                job.jump = working_duration // self.num_clip
+                info_str = ("{}s trailer using {} x {}s clips, "
+                            "taken every {}s".format(
+                                trailer_duration, self.num_clip,
+                                self.duration, job.jump))
+                results.append((True, idx, job.source, info_str))
+        return results
 
     def save_options_as_preset(self, preset_name):
         try:
@@ -97,7 +107,7 @@ class ClipMaker(object):
                 "duration": self.duration,
                 "num_clip": self.num_clip
             }
-            with open(self.config_filename, "w") as config_file:
+            with open(self.config_file_name, "w") as config_file:
                 self.config.write(config_file)
             info_str = ("Saved preset '{}' - start_time: {}; duration: {}; "
                         "num_clip: {}".format(preset_name,
@@ -111,14 +121,29 @@ class ClipMaker(object):
             LOG.warning(info_str)
             return False, info_str
 
+    def add_sources(self, source_paths):
+        results = []
+        for source_path, idx in source_paths:
+            info = check_source(source_path)
+            if info[0]:
+                self.jobs.append(ClipJob(source_path, info[1], info[2]))
+            results.append((info[0], idx, source_path))
+        return results
+
+    def remove_job(self, source_path):
+        for i, job in enumerate(self.jobs):
+            if job.source == source_path:
+                del self.jobs[i]
+                break
+
     def get_options(self):
         return {
-            "source": self.source,
-            "target": self.target,
+            "source": [job.source for job in self.jobs],
+            "target": [job.target for job in self.jobs],
             "start_time": self.start_time,
             "duration": self.duration,
             "num_clip": self.num_clip,
-            "jump": self.jump
+            "jump": [job.jump for job in self.jobs]
             }
 
     def get_presets(self):
@@ -130,10 +155,27 @@ class ClipMaker(object):
                                    self.config[section]["duration"],
                                    self.config[section]["num_clip"]))
 
-    def set_source(self, source_path):
-        self.source = source_path
-        self.source_dir = os.path.dirname(source_path)
-        return self.check_source()
+    def get_preset_options(self, preset_name):
+        try:
+            if preset_name == "Select preset":
+                raise AttributeError
+            return True, (self.config[preset_name]["start_time"],
+                          self.config[preset_name]["duration"],
+                          self.config[preset_name]["num_clip"])
+        except AttributeError:
+            info_str = "Invalid preset"
+            LOG.warning(info_str)
+            return False, info_str
+
+    def set_sources(self, source_paths):
+        results = []
+        self.jobs.clear()
+        for source_path, idx in source_paths:
+            info = check_source(source_path)
+            if info[0]:
+                self.jobs.append(ClipJob(source_path, info[1], info[2]))
+            results.append((info[0], idx, source_path))
+        return results
 
     def set_options(self, start_time, duration, num_clip):
         self.start_time_str = start_time
@@ -141,15 +183,3 @@ class ClipMaker(object):
         self.duration = try_parse_int64(duration)
         self.num_clip = try_parse_int64(num_clip)
         return self.check_options()
-
-    def set_options_with_preset(self, preset_name):
-        try:
-            if preset_name == "Select preset":
-                raise AttributeError
-            return self.set_options(self.config[preset_name]["start_time"],
-                                    self.config[preset_name]["duration"],
-                                    self.config[preset_name]["num_clip"])
-        except AttributeError:
-            info_str = "Invalid preset"
-            LOG.warning(info_str)
-            return False, info_str
--- clipsapp.py
+++ clipsapp.py
@@ -1,7 +1,8 @@
 import logging
+import os.path
 import sys
 
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QMainWindow,
                              QVBoxLayout, QWidget)
 
@@ -15,7 +16,7 @@ from presetwidgetgroup import PresetWidgetGroup
 
 class ClipsApp(QMainWindow):
     ffmpeg_create = pyqtSignal(dict)
-    gfycat_upload = pyqtSignal(str)
+    gfycat_upload = pyqtSignal(list)
 
     def __init__(self):
         super().__init__()
@@ -30,7 +31,9 @@ class ClipsApp(QMainWindow):
 
         hbox_filedialog = QHBoxLayout()
         self._browse_group = BrowseWidgetGroup(self, hbox_filedialog)
-
+        for i in range(hbox_filedialog.count()):
+            hbox_filedialog.setAlignment(hbox_filedialog.itemAt(i).widget(),
+                                         Qt.AlignTop)
         hbox_options = QHBoxLayout()
         self._option_group = OptionWidgetGroup(self, hbox_options)
 
@@ -39,6 +42,8 @@ class ClipsApp(QMainWindow):
 
         hbox_info = QHBoxLayout()
         self._info_group = InfoWidgetGroup(self, hbox_info)
+        for i in range(hbox_info.count()):
+            hbox_info.setAlignment(hbox_info.itemAt(i).widget(), Qt.AlignTop)
 
         vbox = QVBoxLayout()
         vbox.addLayout(hbox_filedialog)
@@ -51,8 +56,7 @@ class ClipsApp(QMainWindow):
         self.show()
 
     def check_options(self):
-        success, __ = self._clip_maker.check_options()
-        return success
+        return all([job.is_valid for job in self._clip_maker.jobs])
 
     def save_preset(self, preset_name):
         success, info = self._clip_maker.save_options_as_preset(preset_name)
@@ -73,38 +77,57 @@ class ClipsApp(QMainWindow):
     def upload(self):
         if self._is_created:
             self._info_group.set_info("INFO: Uploading")
-            self.gfycat_upload.emit(self._clip_maker.target)
+            self.gfycat_upload.emit([job.target
+                                     for job in self._clip_maker.jobs])
         else:
             self._info_group.set_info("ERROR: Trailer not created")
+
+    def remove_job(self, source_path):
+        self._clip_maker.remove_job(source_path)
+
+    def add_sources(self, source_paths):
+        info = self._clip_maker.add_sources(source_paths)
+        for is_valid, idx, source_path in reversed(info):
+            if not is_valid:
+                self._browse_group.remove_item(idx)
+                self._info_group.set_info(
+                    "INFO: Removed invalid source file '{}'".format(
+                        source_path))
 
     def get_presets(self):
         return self._clip_maker.get_presets()
 
-    def set_source(self, source_path):
-        success, info = self._clip_maker.set_source(source_path)
-        if not success:
-            self._info_group.set_info(info)
+    def set_sources(self, source_paths):
+        info = self._clip_maker.set_sources(source_paths)
+        for is_valid, idx, source_path in reversed(info):
+            if not is_valid:
+                self._browse_group.remove_item(idx)
+                self._info_group.set_info(
+                    "INFO: Removed invalid source file '{}'".format(
+                        source_path))
 
     def set_options(self, start_time, duration, num_clip):
-        if (start_time is None or duration is None or num_clip is None or
-                not start_time or not duration or not num_clip):
+        if (any(opt is None for opt in [start_time, duration, num_clip]) or
+                any(not opt for opt in [start_time, duration, num_clip])):
             self._info_group.set_info("ERROR: Missing options")
             return
-        success, info = self._clip_maker.set_options(start_time, duration,
-                                                     num_clip)
-        if success:
-            self._info_group.set_info(info)
-        else:
-            self._info_group.set_info("ERROR: {}".format(info))
+        info = self._clip_maker.set_options(start_time, duration, num_clip)
+        for is_valid, idx, path, info_str in info:
+            if idx == -1:
+                self._info_group.set_info("ERROR: {}".format(info_str))
+                break
+            self._browse_group.set_valid(idx, is_valid)
+            self._info_group.set_info("{}: {} - {}".format(
+                "INFO" if is_valid else "ERROR", os.path.basename(path),
+                info_str))
 
     def set_options_with_preset(self, preset_name):
-        success, info = self._clip_maker.set_options_with_preset(preset_name)
+        success, info = self._clip_maker.get_preset_options(preset_name)
         if success:
-            self._option_group.set_start_time(
-                self._clip_maker.start_time_str)
-            self._option_group.set_duration(str(self._clip_maker.duration))
-            self._option_group.set_num_clip(str(self._clip_maker.num_clip))
-            self._info_group.set_info(info)
+            self._option_group.set_start_time(info[0])
+            self._option_group.set_duration(info[1])
+            self._option_group.set_num_clip(info[2])
+            self.set_options(*info)
         else:
             self._info_group.set_info("ERROR: {}".format(info))
 
@@ -125,7 +148,8 @@ class ClipsApp(QMainWindow):
     @pyqtSlot(str)
     def update_status(self, status):
         self._info_group.set_info(status)
-        self._is_created = True
+        if status == "INFO: Done":
+            self._is_created = True
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
